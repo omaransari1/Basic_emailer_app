a78b9ab1abb157ad9e7eeccd84f93b6f6ad0714c
Omar Ansari <omaransari1@gmail.com>
2017-06-23 02:48:57 -0500

    Call method to send email once user is created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        # Sends email to user when user is created.
+        ExampleMailer.sample_email(@user).deliver
+        
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
