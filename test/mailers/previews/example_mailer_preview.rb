d0b21f4251cdbe82dc5419a58137b904d486d524
Omar Ansari <omaransari1@gmail.com>
2017-06-23 02:18:52 -0500

    Add emailer preview function in the test/mailer directory

--- test/mailers/previews/example_mailer_preview.rb
+++ test/mailers/previews/example_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/example_mailer
+#Preview it: http://localhost:3000/rails/mailers/example_mailer/sample_mail_preview
+
 class ExampleMailerPreview < ActionMailer::Preview
+  def sample_mail_preview
+    ExampleMailer.sample_email(User.first)
+  end
 
 end
